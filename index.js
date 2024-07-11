8ae2bdc2cbefb8284859e5ba45cf3a1751be3020
marcus pedersen <marcus@mpedersen.me>
2024-07-11 19:25:01 +0200

    append paragraph to body

---
+++ index.js
@@ -0,0 +1,8 @@
+// create a new paragraph
+const newParagraph = document.createElement("div");
+
+// add content to the new paragraph
+newParagraph.innerText = "Sell a SaaS";
+
+// append new paragraph to dom
+document.body.appendChild(newParagraph);
